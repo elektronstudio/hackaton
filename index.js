b813a80f4a4699ea8270834769da0c1a8c5d1e5e
kristjanjansen <kristjan.jansen@gmail.com>
2020-12-03 11:33:58 +0100

    separate component

--- index.js
+++ index.js
@@ -4,36 +4,7 @@ import { useUser } from "https://elektronstudio.github.io/live/src/lib/index.js"
 
 // const channel = "hackaton";
 
-const Area = {
-  setup() {
-    const width = 400;
-    const height = 400;
-    const viewBox = `${width / -2} ${height / -2} ${width} ${height}`;
-
-    return { width, height, viewBox };
-  },
-  template: `
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    :width="width"
-    :height="height"
-    :view-box.camel="viewBox"
-    style="
-      display: block;
-      border: 1px solid red;
-      margin-bottom: 16px;
-      position: fixed;
-      top:0;
-      left:0;
-      height:100%;
-      width:100%;
-    "
-  >
-    <circle cx="0" cy="0" r="20" />
-
-  </svg>
-  `,
-};
+import Area from "./src/components/Area.js";
 
 const App = {
   components: { Area },

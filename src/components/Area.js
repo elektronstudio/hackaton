9ab32a27479b41457e3b23096e57464481fd5810
kristjanjansen <kristjan.jansen@gmail.com>
2020-12-03 15:07:57 +0100

    cleanup

--- src/components/Area.js
+++ src/components/Area.js
@@ -1,99 +1,19 @@
-import {
-  ref,
-  provide,
-  inject,
-  computed,
-  watch,
-} from "https://elektronstudio.github.io/live/src/deps/vue.js";
-
-//import { useChannel } from "https://elektronstudio.github.io/foyer2/src/lib/index.js";
-import {
-  socket,
-  createMessage,
-} from "https://elektronstudio.github.io/live/src/lib/index.js";
+import { ref } from "../deps/vue.js";
+import { socket, createMessage, useUser } from "../deps/live.js";
+import { random } from "../deps/foyer2.js";
 
 import { useChannel } from "../lib/channel.js";
 
-const Draggable = {
-  props: ["x", "y"],
-  setup(props, { emit }) {
-    const { mouseX, mouseY } = inject("mouse");
-
-    const mousePressed = ref(false);
-
-    const onMousepress = () => {
-      mousePressed.value = !mousePressed.value;
-    };
-
-    watch([() => mouseX.value, () => mouseY.value], () => {
-      if (mousePressed.value) {
-        emit("drag", { dragX: mouseX.value, dragY: mouseY.value });
-      }
-    });
-
-    const transform = computed(() => `translate(${props.x},${props.y})`);
-
-    const keyOffset = 3;
-
-    document.addEventListener("keydown", (e) => {
-      if (e.key === "ArrowLeft") {
-        emit("drag", { dragX: props.x - keyOffset, dragY: props.y });
-      }
-      if (e.key === "ArrowRight") {
-        emit("drag", { dragX: props.x + keyOffset, dragY: props.y });
-      }
-      if (e.key === "ArrowUp") {
-        emit("drag", { dragX: props.x, dragY: props.y - keyOffset });
-      }
-      if (e.key === "ArrowDown") {
-        emit("drag", { dragX: props.x, dragY: props.y + keyOffset });
-      }
-    });
-
-    return { onMousepress, transform };
-  },
-  template: `
-  <g 
-    :transform="transform"
-    @mousedown="onMousepress"
-    @touchstart="onMousepress"
-    @mouseup="onMousepress"
-    @touchend="onMousepress"
-  >
-    <slot />
-  </g>
-  `,
-};
+import Svg from "./Svg.js";
+import Draggable from "./Draggable.js";
 
 const channel = "hackaton";
 
 export default {
-  components: { Draggable },
+  components: { Svg, Draggable },
   setup() {
-    const width = 10;
-    const height = 1000;
-    const viewBox = `${width / -2} ${height / -2} ${width} ${height}`;
-
-    const svgRef = ref(null);
-    const groupRef = ref(null);
-
-    const mouseX = ref(0);
-    const mouseY = ref(0);
-
     const x = ref(0);
     const y = ref(0);
-
-    const onMousemove = (e) => {
-      let point = svgRef.value.createSVGPoint();
-      point.x = e.changedTouches ? e.changedTouches[0].clientX : e.clientX;
-      point.y = e.changedTouches ? e.changedTouches[0].clientY : e.clientY;
-      let ctm = groupRef.value.getScreenCTM();
-      if ((ctm = ctm.inverse())) {
-        point = point.matrixTransform(ctm);
-      }
-      mouseX.value = point.x;
-      mouseY.value = point.y;
-    };
 
     const onDrag = ({ dragX, dragY }) => {
       x.value = dragX;
@@ -110,54 +30,43 @@ export default {
       socket.send(outgoingMessage);
     };
 
-    provide("mouse", { mouseX, mouseY });
-
     const { users } = useChannel("hackaton");
+    const { userId } = useUser();
 
     return {
-      svgRef,
-      groupRef,
-      width,
-      height,
-      viewBox,
-      onMousemove,
-      mouseX,
-      mouseY,
       onDrag,
       x,
       y,
       users,
+      userId,
     };
   },
   template: `
-  <svg
-    ref="svgRef"
-    xmlns="http://www.w3.org/2000/svg"
-    :awidth="width"
-    :aheight="height"
-    :view-box.camel="viewBox"
-    style="
-      display: block;
-      border: 1px solid blue;
-      margin-bottom: 16px;
-      position: fixed;
-      top: 0;
-      left: 0;
-      height: 100%;
-      width: 100%;
-    "
-    @mousemove="onMousemove"
-    @touchmove="onMousemove"
-  >
-    <g ref="groupRef">
-      <g v-for="user in users">
-        <text :x="user.userX" :y="user.userY" fill="red">{{ user }}</text>
-      </g>
-      <Draggable :x="x" :y="y" @drag="onDrag">
-        <circle r="30" />
-      </Draggable>
+  <Svg>
+    <g v-for="user in users">
+      <circle :cx="user.userX" :cy="user.userY" :r="40" />
+      <text
+        text-anchor="middle"
+        alignment-baseline="central"
+        :x="user.userX"
+        :y="user.userY"
+        fill="white"
+        style="pointer-events: none;"
+      >{{ user.userName }}</text>
+      <circle
+        v-if="user.userId === userId"
+        :cx="user.userX"
+        :cy="user.userY"
+        r="45"
+        fill="none"
+        stroke="black"
+        stroke-width="3"
+      />
     </g>
-  </svg>
+    <Draggable :x="x" :y="y" @drag="onDrag">
+      <circle r="60" fill="rgba(0,0,0,0)" />
+    </Draggable>
+  </Svg>
   <div style="position: fixed; top: 10px; left: 10px">
     {{ x }} / {{ y }}
   </div>
---
+++ src/deps/foyer2.js
@@ -0,0 +1,1 @@
+export * from "https://elektronstudio.github.io/foyer2/src/lib/index.js";

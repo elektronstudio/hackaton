9b355e0aa75dd9a5d2eba4a96364c1f222bf622b
jaanevart <75428997+jaanevart@users.noreply.github.com>
2020-12-04 18:47:30 +0200

    initial commit

--- src/components/Background.js
+++ src/components/Background.js
@@ -6,18 +6,11 @@ export default {
   },
   template: `
     <circle
-      v-for="r in 50"
-      :r="r * 20 + 250"
-      :stroke="'rgba(255,255,255,' + (r / 100) + ')'"
+      v-for="r in 360"
+      :r="r * 30 + 10"
+      :stroke="'rgba(0,0,200,1' + (r / 100) + ')'"
       fill="none"
     />
-    <line 
-      v-for="a in 36"
-      :x1="pol2car(a * 10, 250).x"
-      :y1="pol2car(a * 10, 250).y"
-      :x2="pol2car(a * 10, 1000).x"
-      :y2="pol2car(a * 10, 1000).y"
-      stroke="rgba(255,255,255,0.2)"
-    />
-  `,
+ 
+  `
 };

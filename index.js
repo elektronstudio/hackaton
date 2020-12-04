936683cfcfaf7add96b7cf63b2f9273c76bd0e34
kristjanjansen <kristjan.jansen@gmail.com>
2020-12-04 11:05:22 +0100

    cleanup

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 import { createApp, ref } from "./src/deps/vue.js";
 
+import Background from "./src/components/Background.js";
+import Overlay from "./src/components/Overlay.js";
 import Svg from "./src/components/Svg.js";
 import Users from "./src/components/Users.js";
 import Video from "./src/components/Video.js";
-import Overlay from "./src/components/Overlay.js";
 
 const App = {
-  components: { Video, Svg, Users, Overlay },
+  components: { Background, Overlay, Svg, Users, Video },
   template: `
   <Video />
   <Svg>
+    <Background />
     <Users />
-    <circle v-for="r in 50" :r="r * 20 + 200" :stroke="'rgba(255,255,255,' + (0.3 - r / 100) + ')'" fill="none" />
   </Svg>
   <Overlay />
   `,

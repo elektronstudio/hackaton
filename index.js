76fd4f6669cae4d0567ad7e8e6e2d96039c9cf53
kristjanjansen <kristjan.jansen@gmail.com>
2020-12-04 10:49:10 +0100

    cleanup

--- index.js
+++ index.js
@@ -1,38 +1,19 @@
 import { createApp, ref } from "./src/deps/vue.js";
 
-import { useUser } from "./src/deps/live.js";
-
 import Svg from "./src/components/Svg.js";
 import Users from "./src/components/Users.js";
 import Video from "./src/components/Video.js";
+import Overlay from "./src/components/Overlay.js";
 
 const App = {
-  components: { Video, Svg, Users },
-  setup() {
-    const muted = ref(true);
-    return {
-      ...useUser(),
-      muted,
-    };
-  },
+  components: { Video, Svg, Users, Overlay },
   template: `
-  <Video :muted="muted" />
+  <Video />
   <Svg>
     <Users />
     <circle v-for="r in 50" :r="r * 20 + 200" :stroke="'rgba(255,255,255,' + (0.3 - r / 100) + ')'" fill="none" />
   </Svg>
-  <div
-    style="
-      position: fixed;
-      bottom: 16px;
-      right: 16px;
-      left: 16px;
-      display: flex;
-      justify-content: space-between;
-    ">
-    <button @click="muted = !muted">{{ muted ? 'Unmute' : 'Mute' }}</button>
-    <button @click="onUserNameChange">Change my name</button>
-  </div>  
+  <Overlay />
   `,
 };
 

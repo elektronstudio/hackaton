4627b71b6c347e7a1ee4d786dd55fdb278ad02ab
kristjanjansen <kristjan.jansen@gmail.com>
2020-12-05 16:01:29 +0100

    cleanup

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ export const channel = "hackaton";
 export const channels = [channel, "hackaton_kristjan", "hackaton_hendrik"];
 
 export const channelSources = [
-  "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8",
+  //"https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8",
   ...channels.map(
     (c) => `https://elektron-live.babahhcdn.com/bb1150-le/${c}/index.m3u8`
   ),
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,7 +1,7 @@
 import { ref, computed, watchEffect } from "../deps/vue.js";
 import { socket, createMessage, useUser } from "../deps/live.js";
 
-import { useChannel, useImages, shorten } from "../lib/index.js";
+import { useChannel, useImages, shorten, useAnimation } from "../lib/index.js";
 import Draggable from "./Draggable.js";
 import { channel } from "../../config.js";
 
@@ -52,6 +52,12 @@ export default {
 
     const { userId } = useUser();
 
+    const offset = useAnimation({
+      to: 3,
+      duration: 1 * 1000,
+      alternate: true,
+    });
+
     return {
       onDrag,
       x,
@@ -59,16 +65,24 @@ export default {
       users,
       userId,
       shorten,
+      offset,
     };
   },
   template: `
-  <g v-for="(user, i) in users">
+  <g v-for="user in users.filter(user => user.userId !== userId)">
     <defs>
-      <clipPath :id="'user' + i">
-        <circle :cx="user.userX" :cy="user.userY" :r="50" />
+      <clipPath :id="'clip' + user.userId">
+        <circle :cx="user.userX || 0" :cy="user.userY || 0" :r="50" />
       </clipPath>
     </defs>
-    <circle :cx="user.userX" :cy="user.userY" :r="50" fill="white" :opacity="user.image ? 1 : 0.5" />
+    <circle
+      :cx="user.userX || 0"
+      :cy="user.userY || 0"
+      :r="50"
+      stroke="white"
+      :opacity="user.image ? 0 : 1"
+      fill="rgba(255,255,255,0.1)"
+    />
     <image
       v-if="user.image"
       :href="user.image"
@@ -76,7 +90,60 @@ export default {
       height="100"
       :x="user.userX ? user.userX - 50 : -50"
       :y="user.userY ? user.userY - 50 : -50"
-      :clip-path="'url(#user' + i + ')'"
+      :clip-path="'url(#clip' + user.userId + ')'"
+    />
+    <text
+      text-anchor="middle"
+      alignment-baseline="central"
+      :x="user.userX || 0"
+      :y="user.userY ? user.userY - 75 : -75"
+      fill="white"
+      style="pointer-events: none;"
+    >{{ user.userName }}</text>
+  </g>
+
+  <g v-for="(user, i) in users.filter(user => user.userId === userId)">
+    <defs>
+      <clipPath :id="'clip' + user.userId">
+        <circle :cx="user.userX || 0" :cy="user.userY || 0" :r="50" />
+      </clipPath>
+    </defs>
+    <!--circle
+      v-for="r in 5"
+      :cx="user.userX + offset"
+      :cy="user.userY"
+      :r="45 + (r * 5)"
+      fill="none"
+      stroke="white"
+      stroke-width="2"
+      :opacity="0.5 - (r / 5)"
+    /-->
+    <circle
+      v-for="r in 5"
+      :cx="user.userX"
+      :cy="user.userY"
+      :r="50 + (r * 7)"
+      fill="none"
+      stroke="white"
+      stroke-width="2"
+      :opacity="0.75 - (r / 5)"
+    />
+    <circle
+      :cx="user.userX"
+      :cy="user.userY"
+      :r="50"
+      stroke="white"
+      fill="rgba(255,255,255,0.5)"
+      :opacity="user.image ? 0 : 1"
+    />
+    <image
+      v-if="user.image"
+      :href="user.image"
+      width="100"
+      height="100"
+      :x="user.userX ? user.userX - 50 : -50"
+      :y="user.userY ? user.userY - 50 : -50"
+      :clip-path="'url(#clip' + user.userId + ')'"
     />
     <text
       text-anchor="middle"
@@ -86,21 +153,10 @@ export default {
       fill="white"
       style="pointer-events: none;"
     >{{ user.userName }}</text>
-    <circle
-      v-if="user.userId === userId"
-      :cx="user.userX"
-      :cy="user.userY"
-      r="50"
-      fill="none"
-      stroke="white"
-      stroke-width="3"
-    />
   </g>
+
   <Draggable :x="x" :y="y" @drag="onDrag">
     <circle r="70" fill="rgba(0,0,0,0)" />
   </Draggable>
-  <div style="display: fixed: top: 0, right: 0, background: gray, width: 300px">
-    ...
-  </div>
   `,
 };

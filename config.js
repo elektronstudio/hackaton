221e0cc1f59941dc9fe790059b72f0daa133b009
Kristjan Jansen <kristjan.jansen@gmail.com>
2020-12-05 16:51:35 +0100

    Add background audio (#18)

--- config.js
+++ config.js
@@ -5,3 +5,6 @@ export const channels = [channel, "hackaton_kristjan", "hackaton_hendrik"];
 export const channelSources = channels.map(
   (c) => `https://elektron-live.babahhcdn.com/bb1150-le/${c}/index.m3u8`
 );
+
+export const audioSource =
+  "https://elektron.live/assets/generative-backround-hackaton.mp3";

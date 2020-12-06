5c1aab65a7ff3c7b32aa5680b8a370c44df0282d
kristjanjansen <kristjan.jansen@gmail.com>
2020-12-06 08:10:12 +0100

    rework

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,11 +1,14 @@
 import { ref, computed, watchEffect } from "../deps/vue.js";
 import { createMessage, useUser } from "../deps/live.js";
 import { socket, useChannel, useImages, shorten } from "../lib/index.js";
+
 import Draggable from "./Draggable.js";
+import User from "./User.js";
+
 import { channel } from "../../config.js";
 
 export default {
-  components: { Draggable },
+  components: { Draggable, User },
   setup() {
     const x = ref(0);
     const y = ref(0);
@@ -61,64 +64,9 @@ export default {
     };
   },
   template: `
-  <g v-for="(user, i) in users">
-    <defs>
-      <clipPath :id="'user' + i">
-        <circle :cx="user.userX" :cy="user.userY" :r="50" />
-      </clipPath>
-      <filter id="blur">
-        <feGaussianBlur in="SourceGraphic" stdDeviation="1" />
-      </filter>
-    </defs>
-    <circle
-      :cx="user.userX"
-      :cy="user.userY"
-      :r="50"
-      :fill="user.userId === userId ? 'var(--orange)' : 'white'"
-      :opacity="user.image ? 1 : 0.5"
-    />
-    <image
-      v-if="user.image"
-      :href="user.image"
-      width="100"
-      height="100"
-      :x="user.userX ? user.userX - 50 : -50"
-      :y="user.userY ? user.userY - 50 : -50"
-      :clip-path="'url(#user' + i + ')'"
-    />
-    <!--text
-      v-if="user.userId === userId"
-      filter="url(#blur)"
-      text-anchor="middle"
-      alignment-baseline="central"
-      :x="user.userX"
-      :y="user.userY ? user.userY - 75 : -75"
-      fill="black"
-      style="pointer-events: none; font-weight: bold; font-size: 20px;"
-    >{{ user.userName }}</text-->
-    <text
-      text-anchor="middle"
-      alignment-baseline="central"
-      :x="user.userX"
-      :y="user.userY ? user.userY - 75 : -75"
-      :fill="user.userId === userId ? 'var(--orange)' : 'white'"
-      style="pointer-events: none; font-size: 20px; font-family;"
-    >{{ user.userName }}</text>
-    <circle
-      v-if="user.userId === userId"
-      :cx="user.userX"
-      :cy="user.userY"
-      r="50"
-      fill="none"
-      stroke="rgba(255,127,80,1)"
-      stroke-width="3"
-    />
-  </g>
+  <User v-for="(user, i) in users" :user="user" />
   <Draggable :x="x" :y="y" @drag="onDrag">
     <circle r="70" fill="rgba(0,0,0,0)" />
   </Draggable>
-  <div style="display: fixed: top: 0, right: 0, background: gray, width: 300px">
-    ...
-  </div>
   `,
 };

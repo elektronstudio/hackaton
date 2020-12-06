f03b78a8d230b1937bba14a269283d7cf49a9b4a
kristjanjansen <kristjan.jansen@gmail.com>
2020-12-06 08:04:10 +0100

    cleanup

--- src/components/Users.js
+++ src/components/Users.js
@@ -86,7 +86,7 @@ export default {
       :y="user.userY ? user.userY - 50 : -50"
       :clip-path="'url(#user' + i + ')'"
     />
-    <text
+    <!--text
       v-if="user.userId === userId"
       filter="url(#blur)"
       text-anchor="middle"
@@ -95,7 +95,7 @@ export default {
       :y="user.userY ? user.userY - 75 : -75"
       fill="black"
       style="pointer-events: none; font-weight: bold; font-size: 20px;"
-    >{{ user.userName }}</text>
+    >{{ user.userName }}</text-->
     <text
       text-anchor="middle"
       alignment-baseline="central"

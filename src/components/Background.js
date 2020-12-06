e666fa400ec1c4edea9149bf7d7c9d2038ad8b0e
kristjanjansen <kristjan.jansen@gmail.com>
2020-12-06 11:08:13 +0100

    New background

--- src/components/Background.js
+++ src/components/Background.js
@@ -6,51 +6,37 @@ export default {
     const angle = useAnimation({
       from: 0,
       to: 360,
-      duration: 500 * 1000,
+      duration: 600 * 1000,
       easing: "linear",
     });
     const radius = useAnimation({
       from: 0,
-      to: 300,
-      duration: 1000 * 1000,
+      to: 100,
+      duration: 12 * 1000,
       alternate: true,
       easing: "easeOutExpo",
     });
     return { rotate, pol2car, angle, radius };
   },
   template: `
-    <!-- <circle
-      v-for="r in 50"
-      :r="r * 20 + 250"
-      :stroke="'rgba(255,255,255,' + (r / 100) + ')'"
-      fill="none"
-    /> -->
     <g :transform="rotate(angle)">
-    <line 
+    <!-- <line 
       v-for="a in 72"
-      :x1="pol2car(a * 180, radius).x"
-      :y1="pol2car(a * 180, radius).y"
-      :x2="pol2car(a * 5 + 100, 1500).x"
-      :y2="pol2car(a * 5 + 100, 1500).y"
-      stroke="rgba(255,255,255,0.3)"
-    />
-    <line 
-      v-for="a in 16"
-      :x1="pol2car(a * radius, 180 ).x"
-      :y1="pol2car(a * radius, 180).y"
-      :x2="pol2car(a * 5 - 100, 1500).x"
+      :x1="pol2car(a * 5, radius ).x"
+      :y1="pol2car(a * 5, radius ).y" 
+      :x2="pol2car(a * 5 - 100,  1500).x"
       :y2="pol2car(a * 5 - 100, 1500).y"
-      stroke="rgba(255,255,255,0.2)"
-    />
+      stroke="rgba(255,255,255,0.1)"
+    /> -->
     <line 
-    v-for="a in 16"
-    :x1="pol2car(a * 90, radius).x"
-    :y1="pol2car(a * 90, radius).y"
-    :x2="pol2car(a * 5 - 100, 2500).x"
-    :y2="pol2car(a * 5 - 100, 2500).y"
-    stroke="rgba(255,255,255,0.2)"
+    v-for="a in 180"
+    :x1="pol2car(a * 800 * 2 / 150 , radius ).x"
+    :y1="pol2car(a * 800 * 2/ 150, radius ).y" 
+    :x2="pol2car(a * 2, 1500).x"
+    :y2="pol2car(a * 2, 1500).y"
+    :stroke="'hsla('+ a +', 70%, 60%, 0.2)'"
+    stroke-with="1"
   />
-  
     </g>
   `,
 };

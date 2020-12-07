892434a999215e7cf2966277c93a6892b4bf2690
kristjanjansen <kristjan.jansen@gmail.com>
2020-12-07 11:59:15 +0100

    update streams

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
 export const channel = "hackaton";
 
-export const videoStreamSources = [channel, "hackaton_hendrik"].map(
-  (c) => `https://elektron-live.babahhcdn.com/bb1150-le/${c}/index.m3u8`
-);
+export const videoStreamSources = [
+  channel,
+  "hackaton_kristjan",
+  "hackaton_hendrik",
+].map((c) => `https://elektron-live.babahhcdn.com/bb1150-le/${c}/index.m3u8`);
 
 export const videoFileSources = [
   "https://elektron.live/assets/particleball_small.mp4",

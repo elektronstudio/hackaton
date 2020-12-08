ff6eb2a4b5ac8af2f53cb101afe3db023b31720c
kristjanjansen <kristjan.jansen@gmail.com>
2020-12-08 17:55:41 +0100

    hm

--- src/lib/animation.js
+++ src/lib/animation.js
@@ -12,6 +12,34 @@ export const useAnimation = (customOptions) => {
     ...customOptions,
   };
   const progress = ref(options.from);
+  anime({
+    targets: progress,
+    value: [options.from, options.to],
+    duration: options.duration,
+    easing: options.easing,
+    direction: options.alternate ? "alternate" : null,
+    loop: options.loop,
+  });
+  return progress;
+};
+
+/*
+
+
+import { ref } from "../deps/vue.js";
+import { anime } from "../deps/anime.js";
+
+export const useAnimation = (customOptions) => {
+  const options = {
+    from: 0,
+    to: 1,
+    duration: 10,
+    easing: "linear",
+    loop: true,
+    alternate: false,
+    ...customOptions,
+  };
+  const progress = ref(options.from);
   const animation = anime({
     targets: progress,
     value: [options.from, options.to],
@@ -25,3 +53,5 @@ export const useAnimation = (customOptions) => {
   const play = () => animation.play();
   return { value: progress, play, pause };
 };
+
+*/

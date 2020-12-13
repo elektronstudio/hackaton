36719ac791986fc54fd1df6c51a85389220d69ec
kristjanjansen <kristjan.jansen@gmail.com>
2020-12-13 18:04:30 +0100

    cleanup

--- index2.js
+++ index2.js
@@ -1,13 +1,21 @@
 import { createApp } from "./src/deps/vue.js";
 
-import { Viewport, Background, Map, Circle } from "./src/components2/index.js";
+import { Viewport, Svg, Map, Circle } from "./src/components2/index.js";
 
 const App = {
-  components: { Viewport, Background, Map, Circle },
+  components: { Viewport, Svg, Map, Circle },
   template: `
   <Viewport>
     <Map>
-      <Background />
+      <Svg>
+        <circle
+          v-for="r in 100"
+          :r="r * 10"
+          stroke="rgba(255,255,255,0.3)"
+          fill="none"
+        />
+        <!--line :x2="myX" :y2="myY" stroke="white" /-->
+      </Svg>
       <Circle
         x="-100"
         y="-100"
--- src/components2/index.js
+++ src/components2/index.js
@@ -1,5 +1,5 @@
 export { default as Item } from "./Item.js";
-export { default as Background } from "./Background.js";
+export { default as Svg } from "./Svg.js";
 export { default as Draggable } from "./Draggable.js";
 export { default as Map } from "./Map.js";
 export { default as Viewport } from "./Viewport.js";

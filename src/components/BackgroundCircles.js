bb4e878370dd046e7b72f327bd78341015bbe9f3
kristjanjansen <kristjan.jansen@gmail.com>
2020-12-22 16:05:49 +0200

    cleanup

--- src/components/BackgroundCircles.js
+++ src/components/BackgroundCircles.js
@@ -1,9 +1,33 @@
+import { useAnimation, rgba, map } from "../lib/index.js";
+
 export default {
+  setup() {
+    const radius = useAnimation({
+      to: 2000,
+      duration: 1000 * 10,
+      easing: "easeOutQuart",
+      delay: 1000 * 5,
+    });
+    return { radius, rgba, map };
+  },
   template: `
   <circle
     v-for="r in 200"
     :r="r * 25"
     stroke="rgba(255,255,255,0.15)"
+    fill="none"
+  />
+  <circle
+    cx="-1000"
+    cy="-1000"
+    r="10"
+    fill="red"
+  />
+  <circle
+    cx="-1000"
+    cy="-1000"
+    :r="radius"
+    :stroke="rgba(255,0,0,map(radius,0,2000,1,0.5))"
     fill="none"
   />
   `,
--- src/lib/animation.js
+++ src/lib/animation.js
@@ -6,6 +6,7 @@ export const useAnimation = (customOptions) => {
     from: 0,
     to: 1,
     duration: 10,
+    delay: 0,
     easing: "linear",
     loop: true,
     alternate: false,
@@ -19,39 +20,7 @@ export const useAnimation = (customOptions) => {
     easing: options.easing,
     direction: options.alternate ? "alternate" : null,
     loop: options.loop,
+    delay: options.delay,
   });
   return progress;
 };
-
-/*
-
-
-import { ref } from "../deps/vue.js";
-import { anime } from "../deps/anime.js";
-
-export const useAnimation = (customOptions) => {
-  const options = {
-    from: 0,
-    to: 1,
-    duration: 10,
-    easing: "linear",
-    loop: true,
-    alternate: false,
-    ...customOptions,
-  };
-  const progress = ref(options.from);
-  const animation = anime({
-    targets: progress,
-    value: [options.from, options.to],
-    duration: options.duration,
-    easing: options.easing,
-    direction: options.alternate ? "alternate" : null,
-    loop: options.loop,
-    autoplay: options.autoplay,
-  });
-  const pause = () => animation.pause();
-  const play = () => animation.play();
-  return { value: progress, play, pause };
-};
-
-*/
